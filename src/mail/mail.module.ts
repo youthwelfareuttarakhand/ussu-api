8c920f5a42990f1e0e409e7f18c9e39fd0481b8d
youthwelfareuttarakhand <youthwelfareuttarakhand@gmail.com>
2026-07-28 19:05:10 +0530

    Temporary: re-add diagnostics endpoint to verify published Resend template

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MailService } from "./mail.service";
+import { MailDiagnosticsController } from "./mail-diagnostics.controller";
 
 @Module({
+  controllers: [MailDiagnosticsController],
   providers: [MailService],
   exports: [MailService],
 })
